b176a922fc8de1dc9af5fd34990f0627827bb6ac
jsbray4 <126125074+jsbray4@users.noreply.github.com>
2024-10-06 00:46:38 -0400

    Add files via upload

---
+++ back-end/chat_app-springboot/src/main/java/in/tusharprabhu/chatapp/ChatAppApplication.java
@@ -0,0 +1,16 @@
+package in.tusharprabhu.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the Chat App application.
+ */
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+	}
+
+}
---
+++ back-end/chat_app-springboot/src/main/java/in/tusharprabhu/chatapp/controller/ChatController.java
@@ -0,0 +1,45 @@
+package in.tusharprabhu.chatapp.controller;
+
+import in.tusharprabhu.chatapp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controller class for handling chat-related functionality.
+ */
+@Controller
+public class ChatController {
+
+    /**
+     * Registers a user for chat.
+     * 
+     * param chatMessage The chat message containing the sender's information.
+     * param headerAccessor The SimpMessageHeaderAccessor object used to access session attributes.
+     * return The registered chat message.
+     */
+    // @MessageMapping("/chat.register")
+    // @SendTo("/topic/public")
+    // public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+    //     headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+    //     return chatMessage;
+    // }
+
+    /**
+     * Sends a chat message to all connected users.
+     * 
+     * param chatMessage The chat message to be sent.
+     * return The sent chat message.
+     */
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        System.out.print(chatMessage.getSender() + ": ");
+        System.out.println(chatMessage.getContent());
+        //ChatMessage constructor is never called at all
+        //Backend relies on setters to update timeStamp
+        chatMessage.setTimeStamp();
+        System.out.println(chatMessage.getTimeStamp());
+        return chatMessage;
+    }
+}
